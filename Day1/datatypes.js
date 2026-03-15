232668807a3d4b6157c9d8ee546ca7baa0c74f9a
Dragoş <edragos33@gmail.com>
2026-03-15 12:58:06 +0100

    ex 1 to 5 done

---
+++ Day1/datatypes.js
@@ -0,0 +1,7 @@
+// --- 5. Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable ---
+
+console.log(typeof 'string');
+console.log(typeof true);
+console.log(typeof null);
+console.log(typeof 5);
+console.log(typeof 5.5);
---
+++ Day1/variable.js
@@ -0,0 +1,8 @@
+// --- 4. Create a variable.js file and declare variables and assign string, boolean, undefined and null data types ---
+
+let string = 'This is a string'
+let bool = false
+let undefinedVar
+let nullVar = null
+
+console.log(string, bool, undefinedVar, nullVar)
