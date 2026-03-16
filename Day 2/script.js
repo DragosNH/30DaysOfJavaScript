3edfe8a30eb370ca667bf657bab98595a1362ddb
Dragoş <edragos33@gmail.com>
2026-03-16 08:20:29 +0100

    ex 11 to 15 done

--- Day 2/script.js
+++ Day 2/script.js
@@ -30,3 +30,19 @@ console.log(challenge.split());
 
 // 10. Split the string 30 Days Of JavaScript at the space using split() method.
 console.log(challenge.split(" "));
+
+// 11. 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' split the string at the comma and change it to an array.
+let companies = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon';
+console.log(companies.replace(/,/g, '').split(' '));
+
+// 12. Change 30 Days Of JavaScript to 30 Days Of Python using replace() method.
+console.log(challenge.replace("JavaScript", "Python"));
+
+// 13. What is character at index 15 in '30 Days Of JavaScript' string? Use charAt() method.
+console.log(challenge.charAt(15)); // S
+
+// 14. What is the character code of J in '30 Days Of JavaScript' string using charCodeAt().
+console.log(challenge.charCodeAt('J')); // 51
+
+// 15. Use indexOf to determine the position of the first occurrence of a in 30 Days Of JavaScript.
+console.log(challenge.indexOf('a')); // 4
