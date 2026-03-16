2c1ad6be4f9a2828c74a3709f5c676503362f040
Dragoş <edragos33@gmail.com>
2026-03-16 08:05:04 +0100

    ex 1 to 5 done

---
+++ Day 2/script.js
@@ -0,0 +1,14 @@
+// 1. Declare a variable named challenge and assign it to an initial value '30 Days Of JavaScript'.
+let challenge = '30 Days Of JavaScript';
+
+// 2. Print the string on the browser console using console.log().
+console.log(challenge)
+
+// 3. Print the length of the string on the browser console using console.log().
+console.log("Challenge length: " + challenge.length)
+
+// 4. Change all the string characters to capital letters using toUpperCase() method.
+console.log("To upper case: " + challenge.toUpperCase())
+
+// 5. Change all the string characters to lowercase letters using toLowerCase() method
+console.log("To lower case: " + challenge.toLowerCase())
