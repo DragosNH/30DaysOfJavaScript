bf1e104da59ca29a4fd35b1ae097d9205efd52b3
Dragoş <edragos33@gmail.com>
2026-03-17 08:35:46 +0100

    level 1 done

--- Day 3/levelOne.js
+++ Day 3/levelOne.js
@@ -40,7 +40,7 @@ console.log(5 < 2);
 console.log(5 - 2 == 2);
 console.log(5 === '5');
 
-// 5.Figure out the result of the following comparison expression first without using console.log(). After you decide the result confirm it using console.log()
+// 5.Figure out the result of the following comparison expression first without using console.log(). After you decide the result confirm it using console.log().
 /*
 4 > 3 => true
 4 >= 3 => true
@@ -59,3 +59,45 @@ console.log("--- Ex. 5 ---");
 let py = 'python';
 let jarg = 'jargon';
 console.log(py.length != jarg.length);
+
+// 6.Figure out the result of the following expressions first without using console.log(). After you decide the result confirm it by using console.log().
+/*
+4 > 3 && 10 < 12 => true
+4 > 3 && 10 > 12 => false
+4 > 3 || 10 < 12 => true
+4 > 3 || 10 > 12 => true
+!(4 > 3) => false
+!(4 < 3) => true
+!(false) => true
+!(4 > 3 && 10 < 12) => false
+!(4 > 3 && 10 > 12) => true
+!(4 === '4') => true
+There is no 'on' in both dragon and python => false*/
+
+console.log("--- Ex. 6 ---");
+console.log(!'python'.includes('on'));
+console.log(!'dragon'.includes('on'));
+
+// 7. Use the Date object to do the following activities:
+
+console.log("--- Ex. 7 ---");
+// What is the year today?
+let currentYear = new Date().getFullYear();
+console.log(currentYear);
+// What is the month today as a number? 
+let month = new Date().getMonth();
+console.log(month);
+// What is the day today as a number?
+let day = new Date().getDate();
+console.log(day);
+// What is the date today?
+console.log(`${day}/${month + 1}/${year}`);
+// What is the hours now?
+let hour = new Date().getHours();
+console.log(hour);
+// What is the minutes now?
+let minute = new Date().getMinutes();
+console.log(minute);
+// Find out the numbers of seconds elapsed from January 1, 1970 to now.
+let allSeconds = Date.now();
+console.log(allSeconds);
