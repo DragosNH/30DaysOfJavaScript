a70169155edb906a9f5af4a6dec58f52d1b2ee56
Dragoş <edragos33@gmail.com>
2026-03-22 06:38:14 +0100

    ex 6 to 10 done

--- Day 5/levelOne.js
+++ Day 5/levelOne.js
@@ -36,9 +36,36 @@ console.log(elementsArray.length); // 10
 console.log("--- Ex 4 ---");
 console.log(elementsArray[0]);
 console.log(elementsArray[5]);
-console.log(elementsArray[index]);
+console.log(elementsArray[9]);
 
 // 5.Declare an array called mixedDataTypes, put different data types in the array and find the length of the array. The array size should be greater than 5.
 console.log("--- Ex 5 ---");
 const mixedDataTypes = ["Potato", true, ["yes", "no", "maybe"], 5, "string", "element", 10, "this is a string"];
 console.log(mixedDataTypes.length);
+
+// 6. Declare an array variable name itCompanies and assign initial values Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon.
+const itCompanies = ["Facebook", "Google", "Microsoft", "Apple", "IBM", "Oracle", "Amazon"];
+
+// 7. Print the array using console.log().
+console.log("--- Ex 7 ---");
+console.log(itCompanies);
+
+// 8. Print the number of companies in the array.
+console.log("--- Ex 8 ---");
+console.log(itCompanies.length); // 7
+
+// 9. Print the first company, middle and last company.
+console.log("--- Ex 9 ---");
+console.log(itCompanies[0]);
+console.log(itCompanies[3]);
+console.log(itCompanies[6]);
+
+// 10. Print out each company.
+console.log("--- Ex 10 ---");
+console.log(itCompanies[0]);
+console.log(itCompanies[1]);
+console.log(itCompanies[2]);
+console.log(itCompanies[3]);
+console.log(itCompanies[4]);
+console.log(itCompanies[5]);
+console.log(itCompanies[6]);
