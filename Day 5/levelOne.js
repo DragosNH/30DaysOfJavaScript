04070f0bb333ac27cc81f57435b7ec58d36fb7e3
Dragoş <edragos33@gmail.com>
2026-03-22 06:32:58 +0100

    ex 1 to 5 done

---
+++ Day 5/levelOne.js
@@ -0,0 +1,44 @@
+// --- Level 1 ---
+const countries = [
+  'Albania',
+  'Bolivia',
+  'Canada',
+  'Denmark',
+  'Ethiopia',
+  'Finland',
+  'Germany',
+  'Hungary',
+  'Ireland',
+  'Japan',
+  'Kenya'
+]
+
+const webTechs = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'React',
+  'Redux',
+  'Node',
+  'MongoDB'
+]
+// 1. Declare an empty array.
+const emptyArray = [];
+
+// 2. Declare an array with more than 5 number of elements
+const elementsArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// 3.Find the length of your array.
+console.log("--- Ex 3 ---");
+console.log(elementsArray.length); // 10
+
+// 4.Get the first item, the middle item and the last item of the array.
+console.log("--- Ex 4 ---");
+console.log(elementsArray[0]);
+console.log(elementsArray[5]);
+console.log(elementsArray[index]);
+
+// 5.Declare an array called mixedDataTypes, put different data types in the array and find the length of the array. The array size should be greater than 5.
+console.log("--- Ex 5 ---");
+const mixedDataTypes = ["Potato", true, ["yes", "no", "maybe"], 5, "string", "element", 10, "this is a string"];
+console.log(mixedDataTypes.length);
