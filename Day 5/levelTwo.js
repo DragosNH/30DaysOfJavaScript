98af89e181001f3ad16fd6c5f3370250b5ee0575
Dragoş <edragos33@gmail.com>
2026-03-23 06:32:29 +0100

    Level 2 done

--- Day 5/levelTwo.js
+++ Day 5/levelTwo.js
@@ -65,3 +65,8 @@ if (webTechs.includes("Sass")){
     console.log(webTechs);   
 }
 
+// 6. Concatenate the following two variables and store it in a fullStack variable.
+const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+const backEnd = ['Node','Express', 'MongoDB']
+const fullStack = frontEnd + backEnd;
+console.log(fullStack);
