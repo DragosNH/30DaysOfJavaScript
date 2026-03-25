0d567dcad3ba610abf9b2ac89e129d07a4c62f35
Dragoş <edragos33@gmail.com>
2026-03-25 06:41:51 +0100

    ex 6 to 10 done

--- Day 6/levelOne.js
+++ Day 6/levelOne.js
@@ -91,6 +91,59 @@ for (let i = "*"; i.length <= 8; i+="*"){
 10 x 10 = 100
 */
 
+console.log("--- Ex 6 ---");
 for (let i = 0; i <= 10; i++){
     console.log(`${i} * ${i} = ${i * i}`);
 }
+
+// 6. Using loop print the following pattern:
+/* 
+ i    i^2   i^3
+ 0    0     0
+ 1    1     1
+ 2    4     8
+ 3    9     27
+ 4    16    64
+ 5    25    125
+ 6    36    216
+ 7    49    343
+ 8    64    512
+ 9    81    729
+ 10   100   1000
+*/
+console.log("--- Ex 6 ---");
+console.log("i   i^2   i^3");
+
+for(let i = 0; i<=10; i++){
+    console.log(`${i}   ${i**2}  ${i**3}`);
+}
+
+// 7. Use for loop to iterate from 0 to 100 and print only even numbers
+console.log("--- Ex 7 ---");
+
+for (let i = 0; i <= 100; i++){
+    if(i % 2 === 0){
+        console.log(i);
+    }
+}
+
+// 8. Use for loop to iterate from 0 to 100 and print only odd numbers.
+console.log("--- Ex 8 ---");
+for (let i = 0; i <= 100; i++){
+    if(i % 2 !== 0){
+        console.log(i);
+    }
+}
+
+// 9. Use for loop to iterate from 0 to 100 and print only prime numbers.
+
+// 10. Use for loop to iterate from 0 to 100 and print the sum of all numbers.
+console.log("--- Ex 10 ---");
+
+let sum = 0;
+
+for (let i = 0; i <= 100; i++) {
+    sum += i;
+}
+
+console.log(`The sum of all numbers from 0 to 100 is ${sum}`);
