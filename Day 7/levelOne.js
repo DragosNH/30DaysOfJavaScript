4df5237bb9843deeafe15c49936515cb59bf832f
Dragoş <edragos33@gmail.com>
2026-03-30 06:37:45 +0200

    Ex 6 to 10 from level 1 finished

--- Day 7/levelOne.js
+++ Day 7/levelOne.js
@@ -39,10 +39,59 @@ console.log(areaOfRectangle(5,8));
 // 5. A perimeter of a rectangle is calculated as follows: perimeter= 2x(length + width). Write a function which calculates perimeterOfRectangle.
 console.log("--- Ex 5 ---");
 
- const perimeterOfRectangle = (length, width) =>{
+const perimeterOfRectangle = (length, width) =>{
     let perimeter = 2 * (length + width);
     return `The perimeter of the rectangle if length is: ${length},\nand width is: ${width}\nthe perimeter is: ${perimeter}`;
- }
+}
 
- console.log(perimeterOfRectangle(5, 15));
- 
+console.log(perimeterOfRectangle(5, 15));
+
+// 6. A volume of a rectangular prism is calculated as follows: volume = length x width x height. Write a function which calculates volumeOfRectPrism.
+console.log("--- Ex 6 ---");
+
+const volumeOfRectPrism = (length, width, height) =>{
+    let volume = length * width * height;
+    console.log(`Th vlume is: ${volume}`);
+}
+
+volumeOfRectPrism(5, 8, 10);
+
+// 7. Area of a circle is calculated as follows: area = π x r x r. Write a function which calculates areaOfCircle.
+console.log("--- Ex 7 ---");
+
+const areaOfCircle = r => {
+    let area = Math.PI * r * r;
+    console.log(`The area of circle is: ${area}`);
+}
+
+areaOfCircle(15);
+
+// 8. Circumference of a circle is calculated as follows: circumference = 2πr. Write a function which calculates circumOfCircle.
+console.log("--- Ex 8 ---");
+
+const circumOfCircle = r => {
+    let circumference = 2 * Math.PI * r;
+    console.log(`The circumference is: ${circumference}`);
+}
+
+circumOfCircle(20);
+
+// 9. Density of a substance is calculated as follows:density= mass/volume. Write a function which calculates density.
+console.log("--- Ex 9 ---");
+
+const densityOfSubstance = (mass, volume) => {
+    let density = mass / volume;
+    console.log(`Mass: ${mass} ÷ Volume: ${volume} = Density: ${density}`);
+}
+
+densityOfSubstance(12, 20);
+
+// 10. Speed is calculated by dividing the total distance covered by a moving object divided by the total amount of time taken. Write a function which calculates a speed of a moving object, speed.
+console.log("--- Ex 10 ---");
+
+const calculateSpeed = (distance, time) => {
+    let speed = distance / time;
+    console.log(`${speed}`);
+}
+
+calculateSpeed(12, 0.5);
