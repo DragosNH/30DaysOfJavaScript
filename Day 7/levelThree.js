30cdceb7760dc90020eea94584f9787c231b98ea
Dragoş <edragos33@gmail.com>
2026-04-05 13:39:37 +0200

    ex 8 done

--- Day 7/levelThree.js
+++ Day 7/levelThree.js
@@ -166,3 +166,18 @@ console.log(generateColors('hexa', 3)) // ['#a3e12f', '#03ed55', '#eb3d2b']
 console.log(generateColors('hexa', 1)) // '#b334ef'
 console.log(generateColors('rgb', 3)) // ['rgb(5, 55, 175)', 'rgb(50, 105, 100)', 'rgb(15, 26, 80)']
 console.log(generateColors('rgb', 1)) // 'rgb(33,79, 176)'
+
+// 8. Call your function shuffleArray, it takes an array as a parameter and it returns a shuffled array.
+console.log("--- Ex 8 ---");
+
+const shuffleArray = arr => {
+    for(let i = arr.length -1; i > 0; i--){
+        let j = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+
+    console.log(arr);
+}
+
+const array = [1, 2, 3, 4, 5];
+shuffleArray(array);
