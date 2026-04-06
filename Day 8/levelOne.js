63c23473c6f49535e8185627e15dfe4c4c677176
Dragoş <edragos33@gmail.com>
2026-04-06 06:11:04 +0200

    Level 1 done

--- Day 8/levelOne.js
+++ Day 8/levelOne.js
@@ -11,10 +11,18 @@ console.log(dog);
 // 3. Add name, legs, color, age and bark properties for the dog object. The bark property is a method which return woof woof.
 dog.name = "Barky";
 dog.legs = 4;
-dog.color = "Black";
+dog.color = "White and brown";
 dog.age = 2;
 dog.bark = () => {return "woof woof"};
 
 // 4. Get name, legs, color, age and bark value from the dog object.
 console.log(Object.values(dog));
 
+// 5. Set new properties the dog object: breed, getDogInfo.
+dog.breed = "Beagle";
+
+dog.getInfo = () => {
+    console.log(`Dog: ${Object.values(this.dog)}`);
+}
+
+console.log(dog);
