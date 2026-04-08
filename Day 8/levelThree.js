370f39cb4ff575474539b0e3674e600fe7e1d1c8
Dragoş <edragos33@gmail.com>
2026-04-08 08:01:43 +0200

    level 3 started

---
+++ Day 8/levelThree.js
@@ -0,0 +1,4 @@
+// ------ Level 3 ------
+console.log("--- Level 3 ---");
+
+// 1. Create an object literal called personAccount. It has firstName, lastName, incomes, expenses properties and it has totalIncome, totalExpense, accountInfo,addIncome, addExpense and accountBalance methods. Incomes is a set of incomes and its description and expenses is a set of incomes and its description.
