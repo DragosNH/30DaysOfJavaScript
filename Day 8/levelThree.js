248237629842d4cadabc1c9f64ff45d1d2d57de0
Dragoş <edragos33@gmail.com>
2026-04-08 08:08:14 +0200

    ex 1 from level 3 done

--- Day 8/levelThree.js
+++ Day 8/levelThree.js
@@ -2,3 +2,17 @@
 console.log("--- Level 3 ---");
 
 // 1. Create an object literal called personAccount. It has firstName, lastName, incomes, expenses properties and it has totalIncome, totalExpense, accountInfo,addIncome, addExpense and accountBalance methods. Incomes is a set of incomes and its description and expenses is a set of incomes and its description.
+
+const personAccount = {
+    firstName: "John",
+    lastName: "Doe",
+    incomes: ["Web developpement", "Graphic design"],
+    expenses: ["Food", "Electricity", "Wather"],
+    properties: ["House", "Car", "Garage"],
+    totalIncome: 2000,
+    totalExpense: 1800,
+    accountInfo: "Green",
+    addIncome: 1000,
+    addExpense: 300,
+    accountBalance: 50000
+};
