6efa1727518d539786ab4a57d395cfdc69e25329
Dragoş <edragos33@gmail.com>
2026-04-11 06:22:16 +0200

    ex 2 done

--- Day 9/levelTwo.js
+++ Day 9/levelTwo.js
@@ -1,2 +1,19 @@
 // --------- Level 2 ---------
 console.log("------ Level 2 ------");
+
+const products = [
+    { product: 'banana', price: 3 },
+    { product: 'mango', price: 6 },
+    { product: 'potato', price: ' ' },
+    { product: 'avocado', price: 8 },
+    { product: 'coffee', price: 10 },
+    { product: 'tea', price: '' },
+]
+
+// 1. Find the total price of products by chaining two or more array iterators(eg. arr.map(callback).filter(callback).reduce(callback)).
+console.log("--- Ex 1 ---");
+
+const totalPrice = products.map(price => price.price).filter(price => price > 0).reduce((acc, value) => acc+value)
+console.log(totalPrice);
+
+// 2. 
