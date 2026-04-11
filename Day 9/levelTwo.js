2a22441aa37aa7b43e8c61d049cac4ee95714790
Dragoş <edragos33@gmail.com>
2026-04-11 06:26:13 +0200

    ex 2 done, kinda

--- Day 9/levelTwo.js
+++ Day 9/levelTwo.js
@@ -16,4 +16,11 @@ console.log("--- Ex 1 ---");
 const totalPrice = products.map(price => price.price).filter(price => price > 0).reduce((acc, value) => acc+value)
 console.log(totalPrice);
 
-// 2. 
+// 2. Find the sum of price of products using only reduce reduce(callback)).
+
+const sumPrice = products.reduce((accumulator, currentValue) => {
+    if(products.price > 0){
+        return accumulator + currentValue.value
+    }
+});
+console.log(sumPrice);
