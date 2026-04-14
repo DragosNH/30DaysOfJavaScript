e699084bda73c7fd8bab6a3a7cb279b0041ec07d
Dragoş <edragos33@gmail.com>
2026-04-14 08:43:16 +0200

    ex 1 done

--- Day 10/levelOne.js
+++ Day 10/levelOne.js
@@ -4,3 +4,9 @@ console.log("------ Level 1 ------");
 const a = [4, 5, 8, 9]
 const b = [3, 4, 5, 7]
 const countries = ['Finland', 'Sweden', 'Norway']
+
+// 1. create an empty set.
+console.log("--- Ex 1 ---");
+
+const epmtySet = new Set();
+console.log(epmtySet);
