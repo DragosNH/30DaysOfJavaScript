37f90bd6883889d6dbdb75e078c25185d00544aa
Dragoş <edragos33@gmail.com>
2026-04-14 08:58:43 +0200

    ex 1 from level 2 done

--- Day 10/levelTwo.js
+++ Day 10/levelTwo.js
@@ -1,2 +1,13 @@
 // --------- Level 2 ---------
 console.log("------ Level 2 ------");
+
+// 1. Find a union b.
+console.log("--- Ex 1 ---");
+
+let a = [1, 2, 3, 4, 5]
+let b = [3, 4, 5, 6]
+let c = [...a, ...b]
+
+let C = new Set(c);
+
+console.log(C);
