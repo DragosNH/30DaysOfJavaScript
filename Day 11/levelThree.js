ed44449e79f007f902ff4b00976ded74db0fa4dc
Dragoş <edragos33@gmail.com>
2026-04-16 06:52:26 +0200

    Day 11 finished

--- Day 11/levelThree.js
+++ Day 11/levelThree.js
@@ -2897,3 +2897,47 @@ const convertArrayToObject = arr => {
     }));
 };
 console.log(convertArrayToObject(students))
+
+// 4. Copy the student object to newStudent without mutating the original object. In the new object add the following ?
+/*
+Add Bootstrap with level 8 to the front end skill sets
+Add Express with level 9 to the back end skill sets
+Add SQL with level 8 to the data base skill sets
+Add SQL without level to the data science skill sets
+*/
+console.log("--- Ex 4 ---");
+
+const s = {
+      name: 'David',
+      age: 25,
+      skills: {
+        frontEnd: [
+          { skill: 'HTML', level: 10 },
+          { skill: 'CSS', level: 8 },
+          { skill: 'JS', level: 8 },
+          { skill: 'React', level: 9 }
+        ],
+        backEnd: [
+          { skill: 'Node',level: 7 },
+          { skill: 'GraphQL', level: 8 },
+        ],
+        dataBase:[
+          { skill: 'MongoDB', level: 7.5 },
+        ],
+        dataScience:['Python', 'R', 'D3.js']
+      }
+}
+
+let skillz = {
+   bts: {skill: 'Bootstrap', level: 8}, 
+   exp: {skill: 'Express', level: 9}, 
+   sql: {skill: 'SQL', level: 8}, 
+   ds : 'SQL'
+} 
+
+let copiedStudent = {...s};
+for(let i=0; i<skillz.length; i++){
+   copiedStudent.skills[i].push(skillz[i]);
+}
+
+console.log(copiedStudent)
