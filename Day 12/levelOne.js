e81bf335081a2d1e193d9f0341ccd9678ac8ca94
Dragoş <edragos33@gmail.com>
2026-04-17 06:15:46 +0200

    ex 2 done

--- Day 12/levelOne.js
+++ Day 12/levelOne.js
@@ -10,3 +10,16 @@ let pattern = /\d+/g;
 let income = text.match(pattern).map(x => parseFloat(x)).reduce((sum, x) => sum + x, 0);
 console.log(income)
 
+// 2. The position of some particles on the horizontal x-axis -12, -4, -3 and -1 in the negative direction, 0 at origin, 4 and 8 in the positive direction. Extract these numbers and find the distance between the two furthest particles.
+console.log("--- Ex 2 ---");
+
+const str = "The position of some particles on the horizontal x-axis -12, -4, -3 and -1 in the negative direction, 0 at origin, 4 and 8 in the positive direction.";
+
+const numbers = str.match(/-?\d+/g).map(Number);
+
+const sorted = numbers.sort((a, b) => a - b);
+
+const distance = sorted[sorted.length - 1] - sorted[0];
+
+console.log(sorted);
+console.log(distance);
