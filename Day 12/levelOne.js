32fd07fc54d96495c67965552cb573bc5a5e832b
Dragoş <edragos33@gmail.com>
2026-04-17 06:10:47 +0200

    ex 1 from level one done

--- Day 12/levelOne.js
+++ Day 12/levelOne.js
@@ -1,2 +1,12 @@
 // --------- Level 1 ---------
 console.log("------ Level 1 ------");
+
+// 1. Calculate the total annual income of the person from the following text. ‘He earns 4000 euro from salary per month, 10000 euro annual bonus, 5500 euro online courses per month.’.
+console.log("--- Ex 1 ---");
+
+let text = "He earns 4000 euro from salary per month, 10000 euro annual bonus, 5500 euro online courses per month.";
+
+let pattern = /\d+/g;
+let income = text.match(pattern).map(x => parseFloat(x)).reduce((sum, x) => sum + x, 0);
+console.log(income)
+
