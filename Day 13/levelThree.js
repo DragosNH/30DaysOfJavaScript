694c20de5505703d8efb15dbd2a8ddbd2f29f2db
Dragoş <edragos33@gmail.com>
2026-04-18 06:36:30 +0200

    Day 13 done

---
+++ Day 13/levelThree.js
@@ -0,0 +1,33 @@
+// --------- Level 3 ---------
+console.log("------ Level 3 ------");
+
+// 1. Check the speed difference among the following loops: while, for, for of, forEach.
+const countries = [
+  ['Finland', 'Helsinki'],
+  ['Sweden', 'Stockholm'],
+  ['Norway', 'Oslo']
+]
+
+console.time('Regular for loop')
+for (let i = 0; i < countries.length; i++) {
+  console.log(countries[i][0], countries[i][1])
+}
+console.timeEnd('Regular for loop')
+
+console.time('for of loop')
+for (const [name, city] of countries) {
+  console.log(name, city)
+}
+console.timeEnd('for of loop')
+
+console.time('forEach loop')
+countries.forEach(([name, city]) => {
+  console.log(name, city)
+})
+console.timeEnd('forEach loop')
+
+console.time('for of loop')
+for(country of countries){
+    console.log(country);   
+}
+console.timeEnd('for of loop')
