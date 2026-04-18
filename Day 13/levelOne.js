ebf0801b103047849963381dca1072353129518b
Dragoş <edragos33@gmail.com>
2026-04-18 06:22:35 +0200

    Ex 1 done

--- Day 13/levelOne.js
+++ Day 13/levelOne.js
@@ -1,2 +1,13 @@
 // --------- Level 1 ---------
 console.log("------ Level 1 ------");
+
+// 1. Display the countries array as a table.
+console.log("--- Ex 1 ---");
+
+const countries = [
+    ['Finland', 'Helsinki'],
+    ['Sweden', 'Stockholm'],
+    ['Norway', 'Oslo']
+]
+
+console.table(countries);
