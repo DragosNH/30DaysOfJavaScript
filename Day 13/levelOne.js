a7856f316b5809905ef927af6e45216b6483df0d
Dragoş <edragos33@gmail.com>
2026-04-18 06:25:13 +0200

    Ex 2 done

--- Day 13/levelOne.js
+++ Day 13/levelOne.js
@@ -11,3 +11,9 @@ const countries = [
 ]
 
 console.table(countries);
+
+// 2. Display the countries object as a table.
+console.log("--- Ex 2 ---");
+
+const countriesObj = {west: 'France', south: 'Italy', east: 'Ukraine', north: 'Finland'};
+console.table(countriesObj);
