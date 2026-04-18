4acf641f7089873a5d4da90915afc2440b544876
Dragoş <edragos33@gmail.com>
2026-04-18 06:30:26 +0200

    Ex 1 done

--- Day 13/levelTwo.js
+++ Day 13/levelTwo.js
@@ -1,3 +1,7 @@
 // --------- Level 2 ---------
 console.log("------ Level 2 ------");
 
+// 1. 10 > 2 * 10 use console.assert().
+console.log("--- Ex 1 ---");
+
+console.assert(10 > 2 * 10, '10 is not greater than 2 * 10');
