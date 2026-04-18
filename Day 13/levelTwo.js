87818b7d44420f394b04cb4c869898cf5b4bd36b
Dragoş <edragos33@gmail.com>
2026-04-18 06:31:42 +0200

    Ex 2 done

--- Day 13/levelTwo.js
+++ Day 13/levelTwo.js
@@ -5,3 +5,8 @@ console.log("------ Level 2 ------");
 console.log("--- Ex 1 ---");
 
 console.assert(10 > 2 * 10, '10 is not greater than 2 * 10');
+
+// 2. Write a warning message using console.warn().
+console.log("--- Ex 2 ---");
+
+console.warn("Warning! You have been warned.");
