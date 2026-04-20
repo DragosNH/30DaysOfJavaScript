fca9929ce0682df07a94bee63aab37a0af852888
Dragoş <edragos33@gmail.com>
2026-04-20 08:30:44 +0200

    Day 15 started

---
+++ Day 15/levelOne.js
@@ -0,0 +1,4 @@
+// --------- Level 1 ---------
+console.log("------ Level 1 ------");
+
+// 1. Create an Animal class. The class will have name, age, color, legs properties and create different methods.
