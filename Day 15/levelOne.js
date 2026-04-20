7381a544461d76eac6dd7adb2edc6c7e20bce2d4
Dragoş <edragos33@gmail.com>
2026-04-20 08:36:13 +0200

    Ex 1 from level 1 done

--- Day 15/levelOne.js
+++ Day 15/levelOne.js
@@ -2,3 +2,23 @@
 console.log("------ Level 1 ------");
 
 // 1. Create an Animal class. The class will have name, age, color, legs properties and create different methods.
+console.log("--- Ex 1 ---");
+
+class Animal {
+    constructor(
+        name = "Berry",
+        age = 1,
+        color = "white",
+        legs = 4
+    ){
+        this.name = name;
+        this.age = age;
+        this.color = color;
+        this.legs = legs;
+    }
+}
+
+const berry = new Animal();
+console.log(berry);
+const wiliam = new Animal("Wiliam", 3, "black", 4);
+console.log(wiliam);
