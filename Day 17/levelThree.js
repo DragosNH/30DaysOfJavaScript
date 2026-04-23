f9c8ec0e37a1598ebb35540aaec8a9fe2f2bf532
Dragoş <edragos33@gmail.com>
2026-04-23 08:54:46 +0200

    Day 17 finished

---
+++ Day 17/levelThree.js
@@ -0,0 +1,13 @@
+// --------- Level 3 ---------
+console.log("------ Level 3 ------");
+
+// 1. Create an object called personAccount. It has firstname, lastname, incomes, expenses properties and it has totalIncome, totalExpense, accountInfo,addIncome, addExpense and accountBalance methods. Incomes is a set of incomes and its description and expenses is also a set of expenses and its description.
+
+let personAccount = {
+    firstName: 'John',
+    lastName: 'Doe',
+    incomes: 2000,
+    expenses: 1000,
+    properties: ['House', 'Car', 'Garage']
+}
+
