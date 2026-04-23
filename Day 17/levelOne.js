cc10f5f8f28f2a2c3cdef1aff702a320dd398ae6
Dragoş <edragos33@gmail.com>
2026-04-23 08:44:32 +0200

    Level 1 Complete

--- Day 17/levelOne.js
+++ Day 17/levelOne.js
@@ -1,2 +1,16 @@
 // --------- Level 1 ---------
 console.log("------ Level 1 ------");
+
+// 1. Store you first name, last name, age, country, city in your browser localStorage.
+
+let info = {
+    firstName: "Dragos",
+    lastName: "Enache",
+    age: 4000,
+    country: "France",
+    city: "Somewhere"
+}
+
+const infoText = JSON.stringify(info, undefined, 2);
+localStorage.setItem('User', infoText);
+console.log(localStorage);
