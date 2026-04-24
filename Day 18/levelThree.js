669353d7f4955052a51d6bb22afaa075242f5a64
Dragoş <edragos33@gmail.com>
2026-04-24 06:32:03 +0200

    Day 18 finished

--- Day 18/levelThree.js
+++ Day 18/levelThree.js
@@ -15,4 +15,33 @@ fetch(catsAPI)
     })
     .catch(error => console.log(error))
 
-    
+// 2. Read the countries api and find out the 10 largest countries
+fetch(countriesAPI)
+    .then(response => response.json())
+    .then(data => {
+        const countriesWithArea = data.sort((a, b) => {
+            if (a.area > b.area) return -1;
+            if (a.area < b.area) return 1;
+            return 0;
+        })
+            .map(el => {
+                return {
+                    [el.name]: el.area
+                }
+            }).slice(0, 10);
+
+        console.log(countriesWithArea)
+    })
+    .catch(error => console.log(error))
+
+// 3. Read the countries api and count total number of languages in the world used as officials.
+
+fetch(countriesAPI)
+    .then(response => response.json())
+    .then(data => {
+        const languages = new Set() // Using set helps us to automatically filter distinct values
+        data.map(el => el.languages.forEach(lang => languages.add(lang.name)))
+
+        console.log(`Total number of official languages in the world is ${languages.size}`)
+    })
+    .catch(error => console.log(error))
