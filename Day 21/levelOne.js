e664805f841e070af093c8af5b9d1d36f16c2c5a
Dragoş <edragos33@gmail.com>
2026-04-27 09:10:08 +0200

    Ex 2 finished

--- Day 21/levelOne.js
+++ Day 21/levelOne.js
@@ -2,7 +2,19 @@
 console.log("------ Level 1 ------");
 
 // 1. Create an index.html file and put four p elements as above: Get the first paragraph by using document.querySelector(tagname) and tag name.
+console.log("--- Ex 1 ---");
+
 const firstParagraph = document.querySelector('p');
 console.log(firstParagraph);
 
-// 2. Get each of the the paragraph using document.querySelector('#id') and by their id
+// 2. Get each of the the paragraph using document.querySelector('#id') and by their id.
+console.log("--- Ex 2 ---");
+const firstP = document.querySelector("#first");
+const secondP = document.querySelector("#second");
+const thirdP = document.querySelector("#third");
+const forurthP = document.querySelector("#fourth");
+
+console.log(firstP);
+console.log(secondP);
+console.log(thirdP);
+console.log(firstP);
