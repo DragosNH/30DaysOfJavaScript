500e86152b10f179ada0d791a41dccc6409ae059
Dragoş <edragos33@gmail.com>
2026-04-27 09:07:08 +0200

    Ex 1 done

--- Day 21/levelOne.js
+++ Day 21/levelOne.js
@@ -1,3 +1,8 @@
 // --------- Level 1 ---------
 console.log("------ Level 1 ------");
 
+// 1. Create an index.html file and put four p elements as above: Get the first paragraph by using document.querySelector(tagname) and tag name.
+const firstParagraph = document.querySelector('p');
+console.log(firstParagraph);
+
+// 2. Get each of the the paragraph using document.querySelector('#id') and by their id
