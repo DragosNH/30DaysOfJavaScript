2d61607cb436e6070ab48137c7b6d27baaafc419
Dragoş <edragos33@gmail.com>
2026-05-01 05:54:32 +0200

    button created

--- Day 23/script.js
+++ Day 23/script.js
@@ -2,7 +2,12 @@
 
 const inputBar = document.createElement('input');
 inputBar.type = "number";
-inputBar.name = "number value";
+inputBar.name = "number";
 
+const inputBtn = document.createElement('input');
+inputBtn.type = "submit";
+inputBtn.value = "Generate numbers";
+inputBtn.name = "number";
 
 document.body.appendChild(inputBar);
+document.body.appendChild(inputBtn);
