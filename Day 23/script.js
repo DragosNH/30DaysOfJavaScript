a6f07212c887d8f011c4c7ad5acac4f499309c72
Dragoş <edragos33@gmail.com>
2026-05-01 05:56:27 +0200

    container added

--- Day 23/script.js
+++ Day 23/script.js
@@ -1,5 +1,8 @@
 // Generating numbers and marking evens, odds and prime numbers with three different colors.
 
+const container = document.createElement("div");
+container.classList = "container";
+
 const inputBar = document.createElement('input');
 inputBar.type = "number";
 inputBar.name = "number";
@@ -9,5 +12,6 @@ inputBtn.type = "submit";
 inputBtn.value = "Generate numbers";
 inputBtn.name = "number";
 
-document.body.appendChild(inputBar);
-document.body.appendChild(inputBtn);
+document.body.appendChild(container);
+container.appendChild(inputBar);
+container.appendChild(inputBtn);
