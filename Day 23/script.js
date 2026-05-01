7cfb73d12f7dc8967949cbae7c6dcd72e64b52a5
Dragoş <edragos33@gmail.com>
2026-05-01 05:51:12 +0200

    First project started

---
+++ Day 23/script.js
@@ -0,0 +1,8 @@
+// Generating numbers and marking evens, odds and prime numbers with three different colors.
+
+const inputBar = document.createElement('input');
+inputBar.type = "number";
+inputBar.name = "number value";
+
+
+document.body.appendChild(inputBar);
