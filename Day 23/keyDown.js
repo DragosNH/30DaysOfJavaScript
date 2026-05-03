0e0fe64aea1fc7639d0a4e9b63de14aa6cf770a0
Dragoş <edragos33@gmail.com>
2026-05-03 06:21:59 +0200

    Day 23 finished

--- Day 23/keyDown.js
+++ Day 23/keyDown.js
@@ -4,14 +4,23 @@ const container = document.createElement("div");
 
 const theKey = document.createElement("p");
 theKey.style.fontFamily = "Helvetica";
-theKey.style.color = "green"
+theKey.style.color = "green";
+theKey.style.fontSize = "2em";
+theKey.style.border = "3px solid white";
+theKey.style.boxShadow = "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)";
+theKey.style.width = "350px";
 
 const theKeyCode = document.createElement("p");
 theKeyCode.style.fontFamily = "Helvetica";
-theKeyCode.style.color = "green"
+theKeyCode.style.color = "green";
+theKeyCode.style.fontSize = "2em";
+theKeyCode.style.border = "3px solid white";
+theKeyCode.style.boxShadow = "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)";
+theKeyCode.style.width = "150px";
+
 
 let theText = document.createElement("p");
-theText.innerHTML = "You pressed "
+theText.innerHTML = "You pressed ";
 
 
 document.addEventListener('keydown', function (event) {
