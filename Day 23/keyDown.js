947d7f4819341339646101977b5c78976afd4376
Dragoş <edragos33@gmail.com>
2026-05-03 06:16:11 +0200

    small changes

--- Day 23/keyDown.js
+++ Day 23/keyDown.js
@@ -1,14 +1,25 @@
 // Generating the keyboard code code using even listener.
 
-const title = document.createElement("h1");
-const theKeyCode = document.createElement("h2");
+const container = document.createElement("div");
+
+const theKey = document.createElement("p");
+theKey.style.fontFamily = "Helvetica";
+theKey.style.color = "green"
+
+const theKeyCode = document.createElement("p");
+theKeyCode.style.fontFamily = "Helvetica";
+theKeyCode.style.color = "green"
+
+let theText = document.createElement("p");
+theText.innerHTML = "You pressed "
 
 
 document.addEventListener('keydown', function (event) {
-    title.innerHTML = event.key;
+    theKey.innerHTML = theText.innerHTML + event.key;
     theKeyCode.innerHTML = event.keyCode;
 
 });
 
-document.body.appendChild(title);
-document.body.appendChild(theKeyCode);
+document.body.appendChild(container);
+container.appendChild(theKey);
+container.appendChild(theKeyCode);
