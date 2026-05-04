8df22d59f1e66231ea7085c603fb51313b4a3207
Dragoş <edragos33@gmail.com>
2026-05-04 14:17:00 +0200

    planets added

--- Day 24/script.js
+++ Day 24/script.js
@@ -1,3 +1,29 @@
 const input = document.querySelector(".input");
 const btn = document.querySelector(".btn");
 
+const planets = {
+    mercury:{
+        gravity : 0.38
+    },
+    venus:{
+        gravity : 0.91
+    },
+    earth:{
+        gravity : 1.0
+    },
+    mars:{
+        gravity : 0.38
+    },
+    jupiter:{
+        gravity : 2.34
+    },
+    saturn:{
+        gravity : 0.93
+    },
+    uranus:{
+        gravity : 0.92
+    },
+    neptune:{
+        gravity : 1.12
+    }
+}
